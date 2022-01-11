27a4192a997f11ebe15965b24811e63402a34413
asd147asd147 <asd147asd147@naver.com>
2022-01-11 20:03:48 +0900

    Add Home Windows Info Card Tap

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,4 +4,5 @@ class BankTheme {
     static const Color mainColor = Color(0xffffb7c5);
     static const Color lightColor = Color(0xffffc4d2);
     static const Color darkColor = Color(0xfff1aab8);
+    static const Color brightColor = Color(0xfffff3f5);
 }
